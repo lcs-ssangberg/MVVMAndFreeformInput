2c8344ab33be426f1177279d9288e65d8f4ce153
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-21 11:40:43 -0500

    Initial Commit

---
+++ MVVMAndFreeformInput/MVVMAndFreeformInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMAndFreeformInputApp.swift
+//  MVVMAndFreeformInput
+//
+//  Created by Sebastian on 2025-02-21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMAndFreeformInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
