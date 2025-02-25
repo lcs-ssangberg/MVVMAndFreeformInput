b90f1bd7ebcd597d8292ae53e00723570e043dc7
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-25 08:59:15 -0500

    new lesson, added a array to save results of power

--- MVVMAndFreeformInput/ViewModels/PowerViewModel.swift
+++ MVVMAndFreeformInput/ViewModels/PowerViewModel.swift
@@ -10,55 +10,75 @@ import Foundation
 @Observable
 // VIEW MODEL
 class PowerViewModel {
-   
-   // MARK: Stored properties
-   
-   // Holds whatever the user has typed in the text fields
-   var providedBase: String
-   var providedExponent: String
-   
-   // Holds an appropriate error message, if there was a
-   // problem with input provided by the user
-   var recoverySuggestion: String = ""
-   
-   // MARK: Computed properties
-   // Holds the evaluated power, when the input provided is valid
-   var power: Power? {
-       
-       // First check that the string in providedBase can
-       // be converted into a number, then check that the
-       // value is more than 0
-       guard let base = Double(providedBase) else {
-           recoverySuggestion = "Please provide a numeric value for the base of the power."
-           
-           return nil
-       }
-       
-       // Now check that the string in providedExponent can be
-       // converted into an integer, and that the value is
-       // more than or equal to 1
-       guard let exponent = Int(providedExponent) else {
-           recoverySuggestion = "Please provide an integer value of 0 or greater for the exponent."
-           
-           return nil
-       }
-       
-       // Now that we know the base and exponent have valid values, return the evaluated power
-       recoverySuggestion = "" // No error message
-       return Power(base: base, exponent: exponent)
-       
-   }
-   
-   
-   //MARK: initalizers
-   init(
-       providedBase: String = "",
-       providedExponent: String = "",
-       recoverySuggestion: String = ""
-   ) {
-       self.providedBase = providedBase
-       self.providedExponent = providedExponent
-       self.recoverySuggestion = recoverySuggestion
-   }
-   
+    
+    // MARK: Stored properties
+    
+    // Holds the list of previously computed and evaluated powers
+    var resultHistory: [Power] = []
+    
+    // Holds whatever the user has typed in the text fields
+    var providedBase: String
+    var providedExponent: String
+    
+    // Holds an appropriate error message, if there was a
+    // problem with input provided by the user
+    var recoverySuggestion: String = ""
+    
+    // MARK: Computed properties
+    // Holds the evaluated power, when the input provided is valid
+    var power: Power? {
+        
+        // First check that the string in providedBase can
+        // be converted into a number, then check that the
+        // value is more than 0
+        guard let base = Double(providedBase) else {
+            recoverySuggestion = "Please provide a numeric value for the base of the power."
+            
+            return nil
+        }
+        
+        // Now check that the string in providedExponent can be
+        // converted into an integer, and that the value is
+        // more than or equal to 1
+        guard let exponent = Int(providedExponent) else {
+            recoverySuggestion = "Please provide an integer value of 0 or greater for the exponent."
+            
+            return nil
+        }
+        
+        // Now that we know the base and exponent have valid values, return the evaluated power
+        recoverySuggestion = "" // No error message
+        return Power(base: base, exponent: exponent)
+        
+    }
+    
+    
+    //MARK: initalizers
+    init(
+        providedBase: String = "",
+        providedExponent: String = "",
+        recoverySuggestion: String = ""
+    ) {
+        self.providedBase = providedBase
+        self.providedExponent = providedExponent
+        self.recoverySuggestion = recoverySuggestion
+    }
+    
+    // MARK: Function(s)
+    func saveResult() {
+        
+        // When there is a valid power based on user input...
+        if let power = self.power {
+            
+            // ... save that evaluated power at the top of the history of
+            // results
+            //
+            // NOTE: By inserting the newly evaluated power at the top of
+            //       the array, we ensure the user sees
+            //       the most recent result first.
+            self.resultHistory.insert(power, at: 0)
+        }
+        
+    }
 }
+
